5724e2ee521de8b582d6f83af1869d9b6599190e
Nightblockchain30 <nightblockchain30@gmail.com>
2024-06-05 13:41:46 +0200

    Added json() Middleware from express & Installed morgan for get more info

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
-import express from 'express';
 import 'dotenv/config';
+import express from 'express';
+import morgan from 'morgan';
 
 const server_app = express(); //Esta instancia es nuestra "app express" con todas las funcionalidades del framework
 const port = process.env.PORT || 3000;
+
+//middlewares -> Funciones que nos ayudan a interactuar con el REQ-UEST OBJECT y el RES-PONSE OBJECT 
+server_app.use(morgan('dev'));
+server_app.use(express.json()); // El middleware json() nos ayuda a convertir la petición del cliente a formato JSON para poder trabajar con ella desde el BACKEND 
 
 // root path
 server_app.get('/', (req, res) => {
