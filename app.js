c45f6203b18c9b7c2dab6d4559e155f294583aa7
Nightblockchain30 <nightblockchain30@gmail.com>
2024-06-06 10:50:32 +0200

    Installed Mongoose Dependencies with DB_URL & Added Script -Test Mode

--- app.js
+++ app.js
@@ -2,21 +2,28 @@
 import 'dotenv/config';
 import express from 'express';
 import morgan from 'morgan';
+import mongoose from 'mongoose';
 
 //OBJECTs
-const server_app = express(); //Esta instancia es nuestra "app express" con todas las funcionalidades del framework
+const express_app = express(); //Esta instancia es nuestra "app express" con todas las funcionalidades del framework
+// Variable con la URL de la conexión de la DB
+const URL_DB = (process.env.MODE_ENV = "test" // ¿Estamos en modo Test?
+    ? `mongodb://localhost:27017/ticketing-db-test` // Si estamos en test cogemos esta URL
+    : process.env.URL_DB || "mongodb://localhost:27017/ticketing-db"); // Sino escogeriamos la variable de entorno si existe y sino la última URL
 
+
+mongoose.connect(URL_DB); // Esta conexión llamará a la URL en cuestión de MongoDB
 
 //MIDDLEWAREs -> Funciones que nos ayudan a interactuar con el REQ-UEST OBJECT y el RES-PONSE OBJECT 
-server_app.use(morgan('dev')); // Mejora nuestro LOG y nuestra info en cada petición
-server_app.use(express.json()); // El middleware json() nos ayuda a convertir la petición del cliente a formato JSON para poder trabajar con ella desde el BACKEND 
+express_app.use(morgan('dev')); // Mejora nuestro LOG y nuestra info en cada petición
+express_app.use(express.json()); // El middleware json() nos ayuda a convertir el objeto del cliente a formato JSON para poder trabajar con ella desde el BACKEND 
 
 
 // PATHs
-server_app.get('/', (req, res) => {
+express_app.get('/', (req, res) => {
     res.status(200).send("Holaaa");
 });
 
 
 //EXPORTs
-export default server_app;
+export default express_app;
--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 //IMPORTs
 import 'dotenv/config';
 import app from './app.js';
-import server_app from './app.js';
+import express_app from './app.js';
 
 const port = process.env.PORT || 3000;
 
 
-const server = server_app.listen(port, () => {
+const server = express_app.listen(port, () => {
     console.log(`Envioronment: ${process.env.NODE_ENV}`)
     console.log(`Server is running at http://localhost:${port}`);
 })
