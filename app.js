3e9b00efff461794870378b047cdc21b30d6af97
Nightblockchain30 <nightblockchain30@gmail.com>
2024-06-06 11:34:51 +0200

    ErrorPendingCorrection: Cannot watch URL_DB

--- app.js
+++ app.js
@@ -7,12 +7,15 @@ import mongoose from 'mongoose';
 //OBJECTs
 const express_app = express(); //Esta instancia es nuestra "app express" con todas las funcionalidades del framework
 // Variable con la URL de la conexión de la DB
-const URL_DB = (process.env.MODE_ENV = "test" // ¿Estamos en modo Test?
-    ? `mongodb://localhost:27017/ticketing-db-test` // Si estamos en test cogemos esta URL
-    : process.env.URL_DB || "mongodb://localhost:27017/ticketing-db"); // Sino escogeriamos la variable de entorno si existe y sino la última URL
+const URL_DB =
+    process.env.MODE_ENV === "test" // ¿Estamos en modo Test?
+        ? "mongodb://localhost:27017/ticketing-db-test" // Si estamos en test cogemos esta URL
+        : process.env.URL_DB || "mongodb://localhost:27017/ticketing-db"; // Sino escogeriamos la variable de entorno si existe y sino la última URL
 
-
-mongoose.connect(URL_DB); // Esta conexión llamará a la URL en cuestión de MongoDB
+mongoose
+    .connect(URL_DB) // Esta conexión llamará a la URL en cuestión de MongoDB
+    .then(() => console.log(`Connected to DB : ${URL_DB}`))
+    .catch((err) => console.log(`Failed Connection`, err));
 
 //MIDDLEWAREs -> Funciones que nos ayudan a interactuar con el REQ-UEST OBJECT y el RES-PONSE OBJECT 
 express_app.use(morgan('dev')); // Mejora nuestro LOG y nuestra info en cada petición
